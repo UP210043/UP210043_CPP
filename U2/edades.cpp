ffd18541a4b5dea0b2cacd8d1d63b1cceb72ffc0
Kenya Tali <up210043@alumnos.upa.edu.mx>
2022-09-27 17:56:51 -0500

    correccioón edades

--- U2/edades.cpp
+++ U2/edades.cpp
@@ -3,35 +3,32 @@ using namespace std;
 int main()
 {
     int ed;
-    cout<< "Ingresa tu edad";
-    cin>> ed;
-    if (ed>150 && ed<=0)
+    cout << "Ingresa tu edad";
+    cin >> ed;
+    if (ed > 150 && ed <= 0)
+    {
+        cout << "Edad fuera de rango";
+    }
+    else
+    {
+        if (ed >= 1 && ed <= 30)
         {
-            cout<<"Edad fuera de rango";
+            cout << "Primera edad";
         }
-        else {
-            if(ed>1 && ed<=30)
-            {
-                cout<<"Primera edad";
-            }
-            else 
-            {
-                if (ed>30 || ed<=60)
-                {
-                    cout<<"segunda edad";
-                }
-                else{
-                    if(ed>60 && ed<=90)
-                    {
-                        cout<<"Tercera edad";
-                    }
-                    else
-                    {
-                        cout<< "horas extra";
-                    }
-                }
-            }
-
+        else if (ed > 30 && ed <= 60)
+        {
+            cout << "segunda edad";
         }
+        else if (ed > 60 && ed <= 90)
+        {
+            cout << "Tercera edad";
+        }
+        else
+        {
+            cout << "horas extra";
+        }
+            
+        
+    }
     return 0;
 }
