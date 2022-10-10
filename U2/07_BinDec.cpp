c5109d72dd6ea87afb94bdba74ded85d7cda9183
Kenya Tali <up210043@alumnos.upa.edu.mx>
2022-10-10 16:18:32 -0500

    README

---
+++ U2/07_BinDec.cpp
@@ -0,0 +1,9 @@
+/*Realice un programa que realice la conversión de binario a decimal, solo para números
+enteros mayores a 0. El resultado puede ser mostrado mediante una variable entera o en unconjunto de caracteres.*/
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
